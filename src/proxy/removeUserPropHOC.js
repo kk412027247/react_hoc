84a68ee89d2b30dd4ca896e8c39d859686df75aa
tmd <123456>
2017-08-20 18:00:21 +0800

    remove

---
+++ src/proxy/removeUserPropHOC.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function removeUserProp(WrappedComponent){
+  return function newRender(props){
+    const{user, ...otherProps}=props;
+    return <WrappedComponent {...otherProps} />
+  }
+}
+
+export default removeUserProp;
