b91134d2074f78c6d7a08fb682a515b7c102b6bf
tmd <123456>
2017-08-20 23:10:07 +0800

    0820 2310

--- src/inheritance/removeUserPropHOC.js
+++ src/inheritance/removeUserPropHOC.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-function removeUserProp(WarppedComponent){
-  return class NewComponent extends WarppedComponent{
-    render(){
-      const{user, ...otherProps} = this.props;
-      this.props = otherProps;
-      return super.render()
+// function removeUserProp(WarppedComponent){
+//   return class NewComponent extends WarppedComponent{
+//     render(){
+//       const{user, ...otherProps} = this.props;
+//       this.props = otherProps;
+//       return super.render()
+//
+//     }
+//   }
+// }
 
+function removeUserProp(WrappedComponent){
+  return class NewComponent extends WrappedComponent{
+    render(){
+      const elements = super.render();
+      const{user, ...otherProps} = this.props;
+      return React.cloneElement(elements, otherProps, elements.props.children);
     }
+
   }
 }
 
